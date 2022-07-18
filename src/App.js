b6d2fd39710fb92992a437f7152431a764137d6b
JyldyzMukaeva <mukaeva.jyldyz@gmail.com>
2022-07-18 10:52:25 +0600

    feat: new react app created

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 
-function App() {
-
-    return(
-        <div>
-            <Header/>
-            <MainContent/>
-            <Footer/>
-        </div>
-
-    );
-}
-
-export default App;
+// function App() {
+//
+//     return(
+//         <div>
+//             <Header/>
+//             <MainContent/>
+//             <Footer/>
+//         </div>
+//
+//     );
+// }
+//
+// export default App;
