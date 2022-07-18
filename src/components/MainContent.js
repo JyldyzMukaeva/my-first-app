b39c8f39fbee8420ff6caa5370cdbc65bd4468aa
JyldyzMukaeva <mukaeva.jyldyz@gmail.com>
2022-07-18 10:41:31 +0600

    feat: new react app created

---
+++ src/components/MainContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card";
+
+const MainContent = () => {
+    return(
+        <div className="content">
+            <Card name="iPhone 12 pro" price="70 000"/>
+            <Card name="iPhone 13 pro max" price="90 000"/>
+        </div>
+    )
+}
+
+export default MainContent;
